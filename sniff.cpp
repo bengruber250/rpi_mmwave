e5f9411873bedb26173be25dc5fc984eb2e22a16
Ben Gruber <bengruber250@gmail.com>
2020-02-17 16:33:23 -0600

    Start reading TLVs

--- sniff.cpp
+++ sniff.cpp
@@ -1,7 +1,8 @@
 #include "stdint.h"
 #include "mmw_output.h"
 #include "wiringPi.h"
-#include <wiringSerial.h>
+#include <cstdio>
+#include "wiringSerial.h"
 #include <iostream>
 
 constexpr int BAUD = 921600;
@@ -10,6 +11,7 @@ void setup();
 void waitForMagicWord();
 void loadHeader();
 void printHeader();
+void processTLVs();
 
 static int fd;
 static const char magicWord[8] = { 0x02, 0x01, 0x04, 0x03, 0x06, 0x05, 0x08, 0x07 };
@@ -24,6 +26,7 @@ int main(int argc, char* argv[])
     loadHeader();
     // Print out info.
     printHeader();
+    processTLVs();
     return 0;
 }
 
@@ -61,4 +64,14 @@ void printHeader() {
             header.totalPacketLen,
             header.numDetectedObj,
             header.numTLVs);
+}
+
+void processTLVs() {
+    MmwDemo_output_message_tl tag_length;
+    auto ptr = (uint8_t *) &tag_length;
+    for (int i = 0; i < sizeof(tag_length); ++i) {
+        int next_byte = serialGetchar(fd);
+        *(ptr++) = next_byte;
+    }
+    printf("Tag %d, Length %d", tag_length.type, tag_length.length);
 }
